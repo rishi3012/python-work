8a90f456802fa0f32b4b7ac0a0cdc7ea24cda46c
rishi3012 <50405218+rishi3012@users.noreply.github.com>
2020-03-19 19:13:43 +1100

    Add files via upload

---
+++ UserException.py
@@ -0,0 +1,32 @@
+import random
+from random import randint
+
+
+class Error(Exception):
+    pass
+class ValueTooSmallError(Error):
+    pass
+class ValueTooLargeError(Error):
+    pass
+
+
+
+number = random.randint(0 ,100)
+neg = 0
+
+while True:
+    try:
+        i_num = int(input("Enter a number: "))
+        if i_num < number:
+            raise ValueTooSmallError
+        elif i_num > number:
+            raise ValueTooLargeError
+       
+        break
+    except ValueTooSmallError:
+        print("This value is too small, try again")
+        print()
+    except ValueTooLargeError:
+        print("This value is too large, try again")
+
+print("Congratulations!, You Guesses it correctly")
